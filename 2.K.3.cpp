29da050ad92fdb87f9f658b08f440368820b54cb
Sa2rday <63223296+Sa2rday@users.noreply.github.com>
2023-10-06 15:36:42 +0500

    2 задание Кувшинов

---
+++ 2.K.3.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <vector>
+
+int main() {
+    setlocale(LC_ALL, "RUSSIAN");
+    int n = 4;  // Количество вершин
+    int m = 5;  // Количество рёбер
+
+    // Упакованный массив матрицы инцидентности (пример данных)
+    std::vector<std::vector<int>> incMatrix = {
+        {1, 0, 0, 0, 0},
+        {-1, 1, 0, 0, 0},
+        {0, -1, 1, 0, 0},
+        {0, 0, -1, 1, 0}
+    };
+
+    // Перебор рёбер
+    for (int j = 0; j < m; ++j) {
+        std::cout << "Ребро " << j + 1 << " инцидентно вершинам: ";
+        for (int i = 0; i < n; ++i) {
+            if (incMatrix[i][j] == 1) {
+                std::cout << i + 1 << " (начало) ";
+            }
+            else if (incMatrix[i][j] == -1) {
+                std::cout << i + 1 << " (конец) ";
+            }
+        }
+        std::cout << std::endl;
+    }
+
+    return 0;
+}
